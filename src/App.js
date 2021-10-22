ad8593e2f8b85f38d21b92c359a9269b78a6682b
Dmitriy Stepanov <stdian@mail.ru>
2021-10-23 04:05:32 +0800

    start task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React from 'react'
+
+import Logo from './assets/Logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tickets: [],
+			searchID: '',
+			stopSearch: false,
+			isLoading: true,
+		};
+	}
+
+	async getPackOfTickets() {
+		if (!this.state.stopSearch) {
+			fetch("https://front-test.beta.aviasales.ru/tickets?searchId=" + this.state.searchID)
+			.then(response => {
+				if (response.status === 200) return response.json()
+				return {error: response.status}
+			})
+			.then(json => {
+				if ('error' in json) {
+					this.getPackOfTickets()
+				} else {
+					let tickets = this.state.tickets
+					tickets = tickets.concat(json.tickets)
+					this.setState({tickets: tickets})
+					console.log(this.state.tickets);
+					// console.log(json.tickets);
+					this.setState({isLoading: false})
+					if (json.stop) {
+						this.setState({stopSearch: true})
+					}
+				}
+			})
+		}
+	}
+
+	async getSearchID() {
+		await fetch('https://front-test.beta.aviasales.ru/search')
+		.then(response => response.json())
+		.then(json => {
+			this.setState({searchID: json.searchId})
+		})
+	}
+
+	async componentDidMount() {
+		await this.getSearchID()
+		this.getPackOfTickets()
+	}
+
+	render() {
+		if (this.state.isLoading) {
+			return (<div></div>)
+		}
+		return (
+			<div className="App">
+				<img className="App-logo" src={Logo}></img>
+				<div className="App-content">
+					<div className="App-content-left">
+						
+					</div>
+					<div className="App-content-right">
+						<div className="App-content-tabs">
+						
+						</div>
+						<div className="App-content-tickets">
+							{this.state.tickets.map((ticket => {
+								return (
+									<div className="App-ticket">
+										<div className="App-ticket-header">
+											<p className="App-ticket-price">{ticket.price} Р</p>
+											<img className="App-ticket-carrier-logo" src={'http://pics.avs.io/99/36/' + ticket.carrier + '.png'}></img>
+										</div>
+										
+									</div>
+								)
+							}))}
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App;
